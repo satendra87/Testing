83b7eec39149cb73b92236810318aeeec62fee18
satendra21 <satendra.mc21@gmail.com>
2015-02-09 18:45:15 +0530

    testing

--- Testing/ViewController.m
+++ Testing/ViewController.m
@@ -16,8 +16,17 @@
 
 - (void)viewDidLoad
 {
+    
+    [self testing];
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)testing
+{
+
+    NSLog(@"Testing");
 }
 
 - (void)didReceiveMemoryWarning
